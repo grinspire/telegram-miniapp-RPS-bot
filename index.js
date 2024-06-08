62c0de54db9ff55967c767779bb7f4481361a4df
smlie528 <133619023+smlie528@users.noreply.github.com>
2024-06-08 11:04:42 -0700

    fix: cors error

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename); 
+
+// Configure CORS
+const corsOptions = {
+  origin: '*'
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "view")));
@@ -21,11 +29,6 @@ const server = app.listen(PORT);
 const io = new Server(server);
 
 let room = [];
-
-// const socket = io('https://telegram-miniapp-rps-e8uefmfay-smlie528s-projects.vercel.app', {
-//   transports: ['websocket'],
-//   upgrade: false
-// });
 
 io.on("connection", (socket) => {
   console.log("client connected");
