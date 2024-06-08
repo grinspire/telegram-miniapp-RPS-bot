fbc4356250f712a10e1732ec123fec65e2284f06
smlie528 <133619023+smlie528@users.noreply.github.com>
2024-06-08 10:36:59 -0700

    change port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename); 
 
